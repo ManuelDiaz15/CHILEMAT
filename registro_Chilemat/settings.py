139372d11e85c19d6640b346aa12c36eaaf4d6eb
jeremmyLpg <je.lapaz@duocuc.cl>
2023-05-17 22:54:34 -0400

    Archivo estatico v2

--- registro_Chilemat/settings.py
+++ registro_Chilemat/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/jeremmy/jeremmy.pythonanywhere.com/static'
+STATIC_ROOT = '/home/jeremmy/jeremmy.pythonanywhere.com/core/static'
 
 
 # Default primary key field type
