263c05fe8e12c60cd02855553d0cb525f64a59d1
jeremmyLpg <je.lapaz@duocuc.cl>
2023-05-23 13:07:38 -0400

    web recargar

--- registro_Chilemat/settings.py
+++ registro_Chilemat/settings.py
@@ -76,15 +76,10 @@ WSGI_APPLICATION = 'registro_Chilemat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'ControlMayorista.mysql.pythonanywhere-services.com',
-        'USER': 'ControlMayorista',
-        'PASSWORD': 'Portafolio001D',
-        'NAME': 'ControlMayorista$default',
-        'CHARSET': 'utf8',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'registro_Chilemat.db',
+    }
 }
-
 
 
 # Password validation
